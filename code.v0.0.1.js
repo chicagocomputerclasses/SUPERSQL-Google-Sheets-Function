9a90beb96cec1b6a464a106b26899d0bdc35301e
chicagocomputerclasses <info@chicagocomputerclasses.com>
2020-03-01 01:00:34 -0600

    Create code.v0.0.1.js

---
+++ code.v0.0.1.js
@@ -0,0 +1,37 @@
+function SUPERSQL(sqlAsString,dataAsArray1,...dataAsArrays) {
+  
+  let sqlData = [arrayToObjectOfArrays(dataAsArray1)];
+  dataAsArrays.forEach(ar => sqlData.push(arrayToObjectOfArrays(ar)));
+  
+  let res = alasql(sqlAsString,sqlData);
+  
+  let keys = new Set();
+  res.forEach(r => Object.keys(r).forEach(k => keys.add(k)));
+  
+  let newRes = res.map(r => [...keys].map(k => r[k]));
+  newRes.unshift([...keys]);
+  
+  return newRes;
+  
+}
+
+
+var arrayToObjectOfArrays = function(data){
+
+   data = data.filter(r => !r.every(c => c==""));
+
+   let headers = data.shift();
+  
+    return data.map(r => {
+      
+        let obj = {};
+        
+        r.forEach((cell,i) => {
+          obj[headers[i]] = cell;
+        })
+        
+        return obj;
+    
+    });   
+
+}
