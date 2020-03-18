96443e374e73f3386b78d91a6227192bb1745f1e
chicagocomputerclasses <info@chicagocomputerclasses.com>
2020-03-18 16:36:31 -0500

    Update code.v0.0.1.js

--- code.v0.0.1.js
+++ code.v0.0.1.js
@@ -11,18 +11,32 @@ https://github.com/agershun/alasql
 function SUPERSQL(sqlAsString,dataAsArray1,...dataAsArrays) {
   
   let sqlData = [arrayToObjectOfArrays(dataAsArray1)];
-  dataAsArrays.forEach(ar => sqlData.push(arrayToObjectOfArrays(ar)));
+  
+  // If range/array convert to array of objects otherwise keep as is 
+  dataAsArrays.forEach(ar => Array.isArray(ar) ? sqlData.push(arrayToObjectOfArrays(ar)) : sqlData.push(ar));
   
   let res = alasql(sqlAsString,sqlData);
+    
+  if(res.length === 0){
+     // Check if results exist and return error if no results were found.
+     throw new Error("Statement Returns No Results");
+  } else {
+    // Return the array of results
+    
+    //create unique list of columns ans tore in keysArray variable
+    let keys = new Set();
+    res.forEach(r => Object.keys(r).forEach(k => keys.add(k)));
+    let keysArray = [...keys];
+    
+    // create array of arrays as results
+    let newRes = res.map(r => keysArray.map(k => r[k]));
+    
+    //add header row
+    newRes.unshift(keysArray);
+    
+    return newRes;
   
-  let keys = new Set();
-  res.forEach(r => Object.keys(r).forEach(k => keys.add(k)));
-  let keysArray = [...keys];
-  
-  let newRes = res.map(r => keysArray.map(k => r[k]));
-  newRes.unshift(keysArray);
-  
-  return newRes;
+  }
   
 }
 
@@ -39,7 +53,7 @@ let arrayToObjectOfArrays = function(data){
         
         r.forEach((cell,i) => {
           obj[headers[i]] = cell;
-        });
+        })
         
         return obj;
     
